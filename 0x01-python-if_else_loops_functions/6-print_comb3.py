06157279cc8a97e44a163adf973d085b04427324
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-03-12 13:15:11 -0700

    Added 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:02d}".format(i) + ", {:02d}".format(j), end=", " if i < 9 or j < 8 else "\n")
+for i in range(1, 10):
+    for j in range(i):
+        print("{:02d}".format(j) + ", {:02d}".format(i), end=", " if i < 9 or j < 8 else "\n")
