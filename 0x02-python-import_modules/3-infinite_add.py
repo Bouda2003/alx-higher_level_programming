4d60c9be24c279fb3c654c7139301beecb9654e1
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-03-14 15:22:34 -0700

    Added 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    av = sys.argv#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    av = sys.argv
+    sum = 0
+    for x in range(1, len(av))
+        sum += int(av[x])
+    print("{:d}".format(sum))
