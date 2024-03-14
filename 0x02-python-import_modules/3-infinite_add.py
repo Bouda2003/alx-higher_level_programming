4f4856ed6261ceb6aeb5c3b4f77e09acfcd5d29d
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-03-14 15:24:27 -0700

    Added 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     av = sys.argv
     sum = 0
-    for x in range(1, len(av))
+    for x in range(1, len(av)):
         sum += int(av[x])
     print("{:d}".format(sum))
