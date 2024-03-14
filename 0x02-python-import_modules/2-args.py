192df8e6717ab9ade966584fcd7bf6424c0f3585
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-03-14 15:16:28 -0700

    Added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    av = sys.argv
+    if len(av) - 1 == 0:
+        print("0 arguments:")
+    elif len(av) - 1 == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(len(av) - 1))
+    for x in range(len(av) - 1):
+        print("{:d}: {}".format(x + 1, av[x + 1]))
