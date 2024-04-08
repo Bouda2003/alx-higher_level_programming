d2c9f6da4dd211fd1b6a31d9918f53da59042b0e
Bouda2003 <aute.mohsen@gmail.com>
2024-04-09 00:05:52 +0200

    0-add_integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" this module adds two int or float numbers  """
+
+def add_integer(a, b=98):
+    if type(a) != int and type (a) != float:
+        raise TypeError ("a must be an integer")
+    if type(b) != int and type (b) != float:
+        raise TypeError ("b must be an integer")
+    return int(a) + int (b)
+    
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
