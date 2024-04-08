2b7ccc521a7f9d71afd5ac3fd5789b71a058f4a4
Bouda2003 <aute.mohsen@gmail.com>
2024-04-09 01:57:32 +0200

    0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,9 +2,9 @@
 """ this module adds two int or float numbers  """
 
 def add_integer(a, b=98):
-    if type(a) != int and type (a) != float:
+    if type(a) not in (int, float):
         raise TypeError ("a must be an integer")
-    if type(b) != int and type (b) != float:
+    if type(b) not in (int, float):
         raise TypeError ("b must be an integer")
     return int(a) + int (b)
     
