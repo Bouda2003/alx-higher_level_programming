b5c44898506792e9b61125300604c52882ed088e
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-04-22 15:50:36 -0700

    Added rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,7 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
     @property
     def width(self):
         """Set/get the width of the Rectangle."""
@@ -33,7 +34,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -46,7 +47,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -59,7 +60,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -72,7 +73,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -106,7 +107,7 @@ class Rectangle(Base):
                 - 5th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if args and len(args) != 0:
+        if args and len(args) is not 0:
             a = 0
             for arg in args:
                 if a == 0:
@@ -124,7 +125,7 @@ class Rectangle(Base):
                     self.y = arg
                 a += 1
 
-        elif kwargs and len(kwargs) != 0:
+        elif kwargs and len(kwargs) is not 0:
             for k, v in kwargs.items():
                 if k == "id":
                     if v is None:
