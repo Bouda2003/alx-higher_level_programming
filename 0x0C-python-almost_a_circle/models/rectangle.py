6ec0482718363f517cf70605bd3eadbe5d5e4551
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-04-22 15:02:05 -0700

    Added rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,3 +25,51 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+        def width(self):
+            """Set/get the width of the Rectangle"""
+
+            return self.__width
+
+        def width(self, value):
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            self.__width = value
+
+        def height(self):
+            """Set/get the height of the Rectangle"""
+
+            return self.__height
+
+        def height(self, value):
+            if type(value) != int:
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be > 0")
+            self.__height = value
+
+        def x(self):
+            """Set/get the x coordinate of the Rectangle"""
+
+            return self.__x
+
+        def x(self, value):
+            if type(value) != int:
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("x must be >= 0")
+            self.__x = value
+
+        def y(self):
+            """Set/get the y coordinate of the Rectangle"""
+
+            return self.__y
+
+        def y(self, value):
+            if type(value) != int:
+                raise TypeError("y must be an integer")
+            if value < 0:
+                raise ValueError("y must be >= 0")
+            self.__y = value
