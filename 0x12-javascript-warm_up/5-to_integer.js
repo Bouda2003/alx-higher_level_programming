79ed81f9027ae52f98e3b35e00461296e3cb1884
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-18 16:59:00 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const target = process.argv[2];
+const target = Math.floor(Number(process.argv[2]));
 if (isNaN(target)) {
   console.log('Not a number');
 } else {
-  console.log(Math.floor(Number(target)));
+  console.log(`My number: ${target}`);
 }
