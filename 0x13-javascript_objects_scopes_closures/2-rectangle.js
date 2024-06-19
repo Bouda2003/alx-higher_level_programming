dfbaac5e811378cc184ee029a794a1ed42348964
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-19 15:19:53 -0700

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-    if (w <= 0 && h <= 0) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
       return {};
     }
   }
