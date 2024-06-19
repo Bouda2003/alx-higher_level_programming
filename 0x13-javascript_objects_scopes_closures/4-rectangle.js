2ac8f375fe09e4cb0210813f23b7ce8f79f16c13
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-19 16:03:39 -0700

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
+        }
+        console.log(row);
+      }
+    }
+  }
+
+  rotate () {
+    const temp1 = this.width;
+    const temp2 = this.height;
+    this.width = temp2;
+    this.height = temp1;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
