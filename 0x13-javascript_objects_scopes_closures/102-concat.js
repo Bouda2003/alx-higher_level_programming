e03f3be60fcab0a6d749d05f8b818c9026e71982
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-19 17:46:29 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const files = require('files');
-const a = files.readFileSync(process.argv[2], 'utf8');
-const b = files.readFileSync(process.argv[3], 'utf8');
-files.writeFileSync(process.argv[4], a + b);
+const fs = require('fs');
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
