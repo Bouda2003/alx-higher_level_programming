e848355a05b754116320e3af8bcc333a8b44e2cc
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-23 17:29:26 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <username> <pass> <db_name>
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    DB = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    Cursor = DB.cursor()
+    Cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    states = Cursor.fetchall()
+    for state in states:
+        if state[1][0] == "N":
+            print(state)
