dbd389db84257a11a9e0262c48c9be015ca20289
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-23 18:06:21 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <username> <pass> <db_name> <state_name>
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    DB = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    Cursor = DB.cursor()
+    Cursor.execute("SELECT * \
+            FROM `states` \
+            WHERE BINARY `name` = %s", (sys.argv[4],))
+    states = Cursor.fetchall()
+    for state in states:
+        print(state)
