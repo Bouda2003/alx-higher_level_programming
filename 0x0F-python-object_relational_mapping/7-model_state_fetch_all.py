e37224e0e40d53482b788c2235b6d019bfb722c4
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2024-06-26 14:25:46 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,5 +16,6 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
